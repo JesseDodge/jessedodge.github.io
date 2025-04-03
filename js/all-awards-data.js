718f2b7d0ee2223be71307cf90182b4bc195f913
maxx-css <hellomauriciorivera@gmail.com>
2025-04-03 09:24:07 -0400

    Awards paginated

---
+++ js/all-awards-data.js
@@ -0,0 +1,29 @@
+const allAwardsData = [
+  {
+    title: 'ACL 2024 Best Theme Paper Award',
+    description: 'OLMo: Accelerating the Science of Language Models',
+    url: 'https://www.geekwire.com/2022/ai2-researchers-win-test-of-time-award-that-honors-long-lasting-influence-for-ai-related-papers/',
+  },
+  {
+    title: 'ACL 2024 Best Resource Paper Award',
+    description:
+      'Dolma: An Open Corpus of 3 Trillion Tokens for Language Model Pretraining Research',
+    url: 'https://www.geekwire.com/2022/ai2-researchers-win-test-of-time-award-that-honors-long-lasting-influence-for-ai-related-papers/',
+  },
+  {
+    title: 'GeekWire 2024 Innovation of the Year',
+    description: 'OLMo: Accelerating the Science of Language Models',
+    url: 'https://www.geekwire.com/2022/ai2-researchers-win-test-of-time-award-that-honors-long-lasting-influence-for-ai-related-papers/',
+  },
+  {
+    title: 'ACL 2022 Test-of-Time Paper Award ',
+    description:
+      'Midge: Generating Image Descriptions From Computer Vision Detections',
+    url: 'https://www.geekwire.com/2022/ai2-researchers-win-test-of-time-award-that-honors-long-lasting-influence-for-ai-related-papers/',
+  },
+  {
+    title: 'NAACL 2015 Best Student Paper ',
+    description: 'Retrofitting Word Vectors to Semantic Lexicons',
+    url: 'https://www.geekwire.com/2022/ai2-researchers-win-test-of-time-award-that-honors-long-lasting-influence-for-ai-related-papers/',
+  },
+];
